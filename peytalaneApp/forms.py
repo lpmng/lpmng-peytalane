a92a2a81233b7ad22b92b83d93b7ef39f91e7093
Tjiho Lun <tom.darboux@eisti.fr>
2017-12-25 20:03:41 +0100

    add django forms for inscription page

---
+++ peytalaneApp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class InscriptionExtForm(forms.Form):
+    lastName = forms.CharField(label="Nom",max_length=100)
+    firstName = forms.CharField(label="Prénom",max_length=100)
+    mail = forms.EmailField(label="Mail")
+    username = forms.CharField(label="Pseudo",max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput,label="Mot de passe")
+    passwordConfirm = forms.CharField(widget=forms.PasswordInput,label="Confirmation mot de passe")
+
+class InscriptionEistiForm(forms.Form):
+    username = forms.CharField(label="Pseudo",max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput,label="Mot de passe")
--- peytalaneApp/views.py
+++ peytalaneApp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import InscriptionExtForm,InscriptionEistiForm
 # Create your views here.
 
 def index(request):
     return render(request,'peytalaneApp/index.html',locals())
 
 def inscription(request):
+    form = InscriptionExtForm()
+    formEisti =  InscriptionEistiForm()
     return render(request,'peytalaneApp/inscription.html',locals())
