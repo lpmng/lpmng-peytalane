b2ba2219826d8febba7a841fc460a9c6cd85496d
Tjiho Lun <tom.darboux@eisti.fr>
2018-01-01 10:57:04 +0100

    create admin page for food

--- peytalaneApp/admin.py
+++ peytalaneApp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from peytalaneApp.models_dir.user import User
+from peytalaneApp.models_dir.food import *
+import django
 
-# Register your models here.
+# -- -- -- -- -- -- -- food -- -- -- -- -- -- -- --
+
+class ValueOptionAdmin(admin.ModelAdmin):
+    list_display = ('value',)    
+
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+
+admin.site.register(ValueOption, ValueOptionAdmin)
+admin.site.register(Option, OptionAdmin)
+admin.site.register(Food, FoodAdmin)
+
+# -- -- -- -- -- -- -- user -- -- -- -- -- -- -- --
--- peytalaneApp/models_dir/food.py
+++ peytalaneApp/models_dir/food.py
@@ -4,15 +4,21 @@ from django.db import models
 
 class ValueOption(models.Model):
     value = models.CharField(max_length=50) # EX: oui / non / grande / petite ...
+    def __str__(self):
+        return str(self.value)
 
 class Option(models.Model):
     name = models.CharField(max_length=50) # EX: taille / tomates ? / oignon ? ...
     values = models.ManyToManyField(ValueOption)
+    def __str__(self):
+        return str(self.name)
 
 #templates will be food without user
 class Food(models.Model):
     name = models.CharField(max_length=50) # EX : kebab, pizza 4 fromages, pizza carnivore...
     options = models.ManyToManyField(Option)
     user = models.CharField(max_length=100,blank=True, null=True) #Uid user in the core
+    def __str__(self):
+        return str(self.name)
     
 
