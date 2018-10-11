2a14823a4e91322aa33edcdfec26cffa236fbf6d
Tom Darboux <tom.darboux@laposte.net>
2018-10-11 19:04:22 +0200

    fix csv export view

--- peytalaneApp/views/admin.py
+++ peytalaneApp/views/admin.py
@@ -2,6 +2,8 @@ from django.views import View
 from django.shortcuts import render
 from django.middleware.csrf import CsrfViewMiddleware
 from django.http import HttpResponse
+from django.db.models import Q
+import csv
 
 from peytalaneApp.functions.transaction import Transaction
 from peytalaneApp.models_dir.food import Food
@@ -9,9 +11,52 @@ from peytalaneApp.models_dir.food import ValueOption
 from peytalaneApp.models_dir.user import User
 from peytalaneApp.models_dir.tournament import *
 from peytalaneApp.functions.decorator import IsLogin,IsAdmin
-from peytalaneApp.models_dir.payment import Payment,Payment_option
-
+from peytalaneApp.models_dir.payment import Payment,Payment_option,Payment_option_value
+from peytalaneApp.functions.core import *
 # pages après sélection de l'option choix nourriture
+
+
+
+class Food_csv(View):
+    @IsLogin
+    @IsAdmin
+    def get(self, request,lan_is_reserved,have_foods,have_tournament,is_admin,total, *args, **kwargs):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+        list_food = Payment.objects.filter(Q(product__contains="Pizza"))
+        list_options = list(set(Payment_option.objects.filter(payment__in=list_food)))
+        
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+
+        writer = csv.writer(response)
+
+        first_line = ["Nombre"] + list_options + ["Nom","Commentaire"]
+        writer.writerow(first_line)
+
+        for food in list_food:
+            print("plouf")
+            line = []
+            line.append(1)
+            for option in list_options:
+                try:
+                    line.append(Payment_option_value.objects.get(option=option,payment=food).value)
+                except Payment_option_value.DoesNotExist:
+                    line.append("X")
+            line.append(food.product)
+            line.append(food.comment.strip())
+
+            writer.writerow(line)
+
+        return response
+            
+
+
+    def agregate_food(self,list_food):
+        raise NotImplemented
+
+
+
 
 
 class Food_admin(View):
@@ -21,16 +66,19 @@ class Food_admin(View):
     RENDER_HTML = 'peytalaneApp/admin/food.html'
     @IsLogin
     @IsAdmin
-    def get(self, request,lan_is_reserved,have_foods,have_tournament,is_admin, *args, **kwargs):
+    def get(self, request,lan_is_reserved,have_foods,have_tournament,is_admin,total, *args, **kwargs):
         transactions_list = request.session['transactions']
         list_food = Payment.objects.filter(type_product = "food")
         list_options = Payment_option.objects.all()
         menu_selected_element = "food"
+
+        core = CoreRequest()
+        core.add_session(request)
         return render(request, self.RENDER_HTML, locals())
 
     @IsLogin
     @IsAdmin
-    def put(self, request,lan_is_reserved,have_foods,have_tournament,is_admin, *args, **kwargs):
+    def put(self, request,lan_is_reserved,have_foods,have_tournament,is_admin,total, *args, **kwargs):
         if "id" in request.GET:
             payment_tmp = Payment.objects.get(id = request.GET['id'])
             if "delivered" in request.GET:
@@ -55,14 +103,15 @@ class User_admin(View):
     
     @IsLogin
     @IsAdmin
-    def get(self, request,lan_is_reserved,have_foods,have_tournament,is_admin, *args, **kwargs):
+    def get(self, request,lan_is_reserved,have_foods,have_tournament,is_admin,total, *args, **kwargs):
+        
         list_user = User.objects.all()
         menu_selected_element = "user"
         return render(request, self.RENDER_HTML, locals())
 
     @IsLogin
     @IsAdmin
-    def put(self, request,lan_is_reserved,have_foods,have_tournament,is_admin, *args, **kwargs):
+    def put(self, request,lan_is_reserved,have_foods,have_tournament,is_admin,total, *args, **kwargs):
         if "username" in request.GET:
             user_tmp = User.objects.get(username = request.GET['username'])
             if "admin" in request.GET and username != request.session['username']:
